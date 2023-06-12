7de54bdf6bc8eec760e7b2972647cb9108254755
Ali Elneklawy <89526026+AliElneklawy@users.noreply.github.com>
2023-06-12 03:03:31 +0300

    Update count digits.py

--- count digits.py
+++ count digits.py
@@ -2,6 +2,9 @@ import math
 
 while True:
     n = int(input("Enter the number: "))
-    assert n > 0
+    assert n >= 0
+    if n == 0:
+        print("Number of digits: 1")	
+        continue
     no_digits = math.floor(math.log10(n)) + 1
     print(f"Number of digits: {no_digits}") 
